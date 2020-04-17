3fbc793dd4812e7baaf4a66ee92cb39419015d0f
Nelson <nelsson@gmail.com>
2020-04-17 17:21:37 -0500

    prubas unitarios intermedio 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MedicosComponent } from './intermedio/espias/medicos.component';
+import { MedicoComponent } from './intermedio2/medico/medico.component';
+import { MedicoService } from './intermedio2/medico/medico.service';
+import { HospitalComponent } from './intermedio2/hospital/hospital.component';
 
 @NgModule({
-  declarations: [AppComponent, MedicosComponent],
+  declarations: [AppComponent, MedicosComponent, MedicoComponent, HospitalComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [MedicoService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
